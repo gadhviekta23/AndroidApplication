29bc3b2a595a27ee27ce8a379b04397c557bd512
meet <kalolameet123@gmail.com>
2021-04-07 16:21:29 +0530

    five commint

--- app/src/main/java/com/example/androidapplication/HomeAdapter.java
+++ app/src/main/java/com/example/androidapplication/HomeAdapter.java
@@ -1,31 +1,52 @@
 package com.example.androidapplication;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class HomeAdapter extends RecyclerView.Adapter {
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
+    Context context;
+    private LayoutInflater inflater;
+    ArrayList<HomeItem> homeItems;
+
     public HomeAdapter(Context context, ArrayList<HomeItem> homeItems) {
 
+        this.context = context;
+        this.inflater = LayoutInflater.from(context);
+        this.homeItems = homeItems;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = inflater.inflate(R.layout.home_itemone,viewGroup,false);
+
+        return new  ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull HomeAdapter.ViewHolder holder, int position) {
 
     }
+
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView h_text;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            h_text =itemView.findViewById(R.id.h_text);
+        }
     }
 }
