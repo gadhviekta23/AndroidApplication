c16d686f676a2e03e972d5e06feca464e99917d1
meet <kalolameet123@gmail.com>
2021-04-08 14:29:17 +0530

    recyclerview_hometwo commint meet

---
+++ app/src/main/java/com/example/androidapplication/HomeAdapterOne.java
@@ -0,0 +1,38 @@
+package com.example.androidapplication;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class HomeAdapterOne extends RecyclerView.Adapter<HomeAdapterOne.ViewHolder> {
+
+    public HomeAdapterOne(Context context, ArrayList<HomeItem_two> homeItem1) {
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
