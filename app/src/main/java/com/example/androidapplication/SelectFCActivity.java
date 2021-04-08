024ecdebc45802170ccfc69a19987073bc57aadd
chandani <chandanidhorajiya86@gmail.com>
2021-04-08 11:20:03 +0530

    activity changes

--- app/src/main/java/com/example/androidapplication/SelectFCActivity.java
+++ app/src/main/java/com/example/androidapplication/SelectFCActivity.java
@@ -2,13 +2,36 @@ package com.example.androidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SelectFCActivity extends AppCompatActivity {
+
+    CircleImageView circleImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_f_c);
+        circleImageView = findViewById(R.id.cag_img1);
+
+        circleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), HomePageActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
+
 }
