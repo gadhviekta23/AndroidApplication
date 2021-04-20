f0eca27d73c8ad6c4eaf5ddd4086d874b175fba6
meet <kalolameet123@gmail.com>
2021-04-20 13:00:31 +0530

    HomeAdapterOne commint meet

---
+++ app/src/main/java/com/example/androidapplication/Activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidapplication.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidapplication.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
