c4e1c6f21315c1786edb0b63715f3b5555fac731
VishalK <vsinha224@gmail.com>
2021-04-23 11:27:00 +0530

    loginactivity layout change

--- app/src/main/java/com/example/androidapplication/Activity/LoginActivity.java
+++ app/src/main/java/com/example/androidapplication/Activity/LoginActivity.java
@@ -14,17 +14,20 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class LoginActivity extends AppCompatActivity {
     ImageView image;
+    TextView text_welcome, text_dw;
+    CircleImageView img_facebbok, img_google;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        TextView text_welcome,text_dw;
-        CircleImageView img_facebbok,img_google;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         image = findViewById(R.id.image);
         text_welcome = findViewById(R.id.text_welcome);
         text_dw = findViewById(R.id.text_dw);
         img_facebbok = findViewById(R.id.img_facebbok);
         img_google = findViewById(R.id.img_google);
+
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
