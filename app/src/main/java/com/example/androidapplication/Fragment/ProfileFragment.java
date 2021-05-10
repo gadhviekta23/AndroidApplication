12e21fb8221dcc54f6709d71c34c16a8ec46ef67
chandani <chandanidhorajiya86@gmail.com>
2021-05-10 11:39:27 +0530

    Add_pin

---
+++ app/src/main/java/com/example/androidapplication/Fragment/ProfileFragment.java
@@ -0,0 +1,16 @@
+package com.example.androidapplication.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.androidapplication.R;
+
+public class ProfileFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
